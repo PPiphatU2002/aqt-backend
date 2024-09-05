6ef9e06e8cc7fa93468667bf38eace7a7dab44c3
Piphat <Piphat.u64@rsu.ac.th>
2024-09-05 16:43:57 +0700

    add new auths

---
+++ controller/auths.js
@@ -0,0 +1,130 @@
+require("dotenv").config();
+
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const secret = process.env.secret;
+const { connection } = require("../database");
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    connection.query("SELECT * FROM `employees` WHERE `email` = ?", [email], async function (err, results) {
+      if (err) {
+        console.error("Login Error", err);
+        return res.status(500).json({ message: "Internal Server Error" });
+      }
+
+      const employeeData = results[0];
+
+      if (!employeeData) {
+        return res.status(400).json({ message: "Login Failed (Invalid E-Mail Or Password)" });
+      }
+
+      const match = await bcrypt.compare(password, employeeData.password);
+      if (!match) {
+        return res.status(400).json({ message: "Login Failed (Invalid E-Mail Or Password)" });
+      }
+
+      const token = jwt.sign({ email: employeeData.email, no: employeeData.no }, secret, { expiresIn: "3h" });
+      res.cookie("token", token, {
+        maxAge: 3600000,
+        secure: process.env.NODE_ENV === "production",
+        httpOnly: true,
+        sameSite: "none",
+      });
+
+      res.json({ success: true, token: token });
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+exports.refresh = async (req, res) => {
+  try {
+    const token = req.cookies.token;
+    // console.log("token", token);
+
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const decoded = jwt.verify(token, secret);
+    const [results] = await connection.promise().query("SELECT * FROM employees WHERE no = ?", decoded.no);
+    const employeeData = results[0];
+
+    if (!employeeData) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    res.json({
+      employee: {
+        no: employeeData.id,
+        fname: employeeData.fname,
+        lname: employeeData.lname,
+        email: employeeData.email,
+        ranks_id: employeeData.ranks_id,
+      },
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+exports.logout = async (req, res) => {
+  res.clearCookie("token");
+  res.json({ result: "Success" , message: "Logout Successful" });
+}
+
+exports.register = async (req, res) => {
+  try {
+    const { email, password, fname, lname, ranks_id, phone, created_date, updated_date  } = req.body
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userData = {
+      email,
+      password: hashedPassword,
+      fname,
+      lname,
+      ranks_id,
+      phone,
+      created_date,
+      updated_date,
+    }
+    connection.query("INSERT INTO employees SET ?", 
+      userData, function (err, results) {
+        if (err) {
+          console.error("Employee Creation Failed", err);
+          return res.status(500).json({ message: "Internal Server Error" });
+        }
+        res.json({ message: "User Created", results });
+    }
+    );
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+exports.checkEmailDuplicate = (req, res) => {
+  const { email } = req.body;
+
+  connection.query('SELECT COUNT(*) AS count FROM employees WHERE email = ?', 
+    [email], function (err, results) {
+      if (err) {
+          console.error('Error Checking E-mail:', err);
+          res.status(500).json({ message: 'Internal Server Error' });
+      } else {
+          if (results[0].count > 0) {
+              res.status(400).json({ message: 'E-mail Already Exists' });
+          } else {
+              res.status(200).json({ message: 'E-mail Is Available' });
+          }
+      }
+    }
+  );
+};
